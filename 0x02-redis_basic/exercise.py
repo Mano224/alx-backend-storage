7ca399d7f7ba8d371416f4b613ab8bd323df0029
Mano224 <Mano224@example.com>
2024-11-20 09:32:03 +0200

    s

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,2 +1,20 @@
-
+#!/usr/bin/env python3
 import uuid
+import redis
+from typing import Union
+
+class Cache:
+    """
+        Initialize the Cache class by creating a Redis client instance and flushing the database.
+    """
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        rando_key = str(uuid.uuid4())
+        self._redis.set(rando_key, data)
+        return rando_key
+
+        
